a8c7d0152b0f6f2bf975e7f628a62697495fda53
Fathir Ahmed Shishir <mfa.shishir@gmail.com>
2022-07-07 22:54:37 +0200

    status add

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,13 +3,20 @@ import { useEffect } from 'react';
 import { useDispatch,useSelector  } from 'react-redux';
 import {fetchProducts} from "../Store/ProductSlice"
 import Product from './Product';
-
+import { STATUSES } from '../Store/ProductSlice';
 const Home = () => {
     const dispatch = useDispatch()
-    const { data: products, status } = useSelector((state) => state.product);
+    const { data: products, status  } = useSelector((state) => state.product);
     useEffect(()=>{
         dispatch(fetchProducts())
     },[])
+    
+    if (status === STATUSES.LOADING) {
+        return <h2 className='text-xl text-center'>Loading....</h2>;
+    }
+    if (status === STATUSES.ERROR) {
+        return <h2>Something went wrong!</h2>;
+    }
     return (
         <div >
             <h1 className='text-center text-xl'> Cart products with redux</h1>
--- src/Store/ProductSlice.js
+++ src/Store/ProductSlice.js
@@ -1,10 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
 export const STATUSES = Object.freeze({
-    IDLE : 'idle',
-    ERROE : 'error',
-    LOADING : 'loading' 
-})
+    IDLE: 'idle',
+    ERROR: 'error',
+    LOADING: 'loading',
+});
+
 
 const productSlice = createSlice({
     name : 'product',
@@ -16,28 +18,29 @@ const productSlice = createSlice({
         setProducts(state,action){
             state.data =action.payload
         },
-        setStatus(state,action){
-            state.status = action.payload
-        }
+         setStatus(state, action) {
+            state.status = action.payload;
+         },
     }
 
 })
 
-export const {setProducts, setStatus}= productSlice.actions
+export const {setProducts, setStatus }= productSlice.actions
 export default productSlice.reducer
 
 export function fetchProducts (){
     return async function fetchProductThunk(dispatch, getState)
     {
-        dispatch(setStatus(STATUSES.LOADING))
+        dispatch(setStatus(STATUSES.LOADING));
         try{
        const res = await fetch('https://fakestoreapi.com/products');
        const data = await res.json();
-       dispatch(setProducts(data))   
+       dispatch(setProducts(data)) 
+       dispatch(setStatus(STATUSES.IDLE));  
         }
         catch(error){
             console.log(error)
-            dispatch(setStatus(STATUSES.ERROE))
+            dispatch(setStatus(STATUSES.ERROR));
         }
     }
 }
